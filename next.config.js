5822b8ee4bdd2b1272443737bc88b240f021f4de
Vermakashish001 <kashish854104@gmail.com>
2025-07-13 19:59:56 +0530

    chore: Update next.config.js with output and trailingSlash settings; add netlify.toml for build configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     domains: ['images.unsplash.com', 'via.placeholder.com', 'picsum.photos'],
   },
 }
